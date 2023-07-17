494178a03dffacb876e0dd3191e7e53738a9cd45
Thamizharasan <121797558+vrthamizharasan@users.noreply.github.com>
2023-07-17 20:14:07 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -8,5 +8,5 @@ terraform {
 
 provider "aws" {
   region = "us-east-1"
-  shared_credentials_files = ["/home/ansadmin/.aws/credentials"]
+  shared_credentials_files="/home/ansadmin/.aws/credentials"
 } 
